76a12340f9a8105ebc83b4d5e7d5906d35e7bdd0
ProbhatDeuri <probhat321@gmail.com>
2020-05-07 13:03:45 +0530

    API start page edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    API For CRUD
+                    Rental API- API for Rental Application
                 </div>
             </div>
         </div>
